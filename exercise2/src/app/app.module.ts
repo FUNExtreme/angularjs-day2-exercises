ca766b71c75b0abc29af9d7fef1ff92843f9a8f3
Robin Maenhaut <robin.maenhaut@realdolmen.com>
2018-03-19 15:32:07 +0100

    Exercise 2 solution

--- exercise2/src/app/app.module.ts
+++ exercise2/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { module } from 'angular';
+import 'angular-messages';
+import 'angular-material';
 
 import "./app.scss";
 
@@ -8,11 +10,11 @@ import { RealPizzasOnlyFilter } from './filter';
 import { FocusDirective } from './component';
 import { PizzaDirective } from './component/pizza/pizza.directive';
 
-
-module('app', [])
+module('app', ['ngMaterial', 'ngMessages'])
   .controller(RootController.iid, RootController)
   .controller(PizzeriaController.iid, PizzeriaController)
   .service(PizzaService.iid, PizzaService)
   .filter(RealPizzasOnlyFilter.iid, RealPizzasOnlyFilter.filter)
   .directive(FocusDirective.iid, () => new FocusDirective())
-  .directive(PizzaDirective.iid, () => new PizzaDirective());  
+  .directive(PizzaDirective.iid, () => new PizzaDirective()); 
+  
--- exercise2/src/app/controller/root.controller.ts
+++ exercise2/src/app/controller/root.controller.ts
@@ -2,12 +2,18 @@ import { IController } from "angular";
 
 export class RootController implements IController {
   static iid: string = "RootController";
+  static $inject = ['$mdSidenav'];
+
   public title: string;
 
-  constructor() {
+  constructor(private $mdSidenav) {
   }
 
   $onInit(): void {
     this.title = 'AngularJS Course';
+  }
+
+  toggleMenu(): void {
+    this.$mdSidenav('left').toggle();
   }
 }
